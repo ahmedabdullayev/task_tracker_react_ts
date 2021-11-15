391ceda04cf5611ce2c4d571f35a68815cb9ca19
ahmedabdullayev <ahmed.abdull2016@gmail.com>
2021-11-15 10:27:33 +0200

    onClick event

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,13 @@
+import React from "react";
+
 interface props {
     color: string;
     text: string;
+    onClick: React.MouseEventHandler<HTMLButtonElement>;
 }
-const Button = ({color, text} : props) => {
+const Button = ({color, text, onClick} : props) => {
     return (
-        <button style={{backgroundColor: color}} className={'btn'}>{text}</button>
+        <button onClick={onClick} style={{backgroundColor: color}} className={'btn'}>{text}</button>
     );
 };
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,10 +4,13 @@ interface props {
     title: string;
 }
 const Header = ({title}:props) => {
+    const onClick = () => {
+      console.log("click")
+    }
     return (
         <header className={'header'}>
             <h1>Task Tracker {title}</h1>
-            <Button color={'green'} text={'Add'}/>
+            <Button color={'green'} text={'Add'} onClick={onClick}/>
         </header>
     );
 };
